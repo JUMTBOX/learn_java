7394ec1cc6cf1e2e560eddfc16f3c03809a71e18
JUMTBOX <yjey12@naver.com>
2023-11-10 15:15:38 +0900

    ...

--- firstclass/NumberExample.java
+++ firstclass/NumberExample.java
@@ -1,3 +1,5 @@
+package firstclass;
+
 public class NumberExample {
     public static void main(String[] args) {
         System.out.println(37);
---
+++ firstclass/StringExample.java
@@ -0,0 +1,9 @@
+package firstclass;
+
+public class StringExample {
+    public static void main(String[] args) {
+        System.out.println("나는 문자열");
+        //홑 따옴표에는 여러 문자가 들어갈 수 없다.
+        System.out.println('나'); 
+    }
+}
