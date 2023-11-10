5fc8a05fe539909991092a2fe25be44360950675
JUMTBOX <yjey12@naver.com>
2023-11-10 16:53:22 +0900

    ...

---
+++ fourth/Wrapper.java
@@ -0,0 +1,17 @@
+package fourth;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        Integer num = 1;
+        Long num2 = 2L;
+        Float num3 = 3f;
+        Double num4 = 4d;
+        Boolean yes = true;
+
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(yes);
+    }
+}
